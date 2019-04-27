27f6f65bee7a9aebfeb55a601b54e10616412892
Crowdbotics <team@crowdbotics.com>
2019-04-27 23:22:52 +0000

    initial commit

---
+++ android/app/src/main/java/com/healing_fire_deliver_2798/MainActivity.java
@@ -0,0 +1,15 @@
+package com.healing_fire_deliver_2798;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "healing_fire_deliver_2798";
+    }
+}
